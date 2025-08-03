302fbce15df29bb05cc5aabda3f02e0c4d07a954
francoycc <francoycc@gmail.com>
2025-08-03 19:55:59 -0300

    APP iniciada

---
+++ SigemiApplication/src/main/java/com/sigemi/SigemiApplication/SigemiApplication.java
@@ -0,0 +1,13 @@
+package com.sigemi.SigemiApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SigemiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SigemiApplication.class, args);
+	}
+
+}
---
+++ SigemiApplication/src/test/java/com/sigemi/SigemiApplication/SigemiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sigemi.SigemiApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SigemiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
