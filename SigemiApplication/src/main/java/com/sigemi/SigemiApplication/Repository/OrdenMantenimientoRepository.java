a6801fc8ca221268ec9dc8119f0052950afe1d1f
francoycc <francoycc@gmail.com>
2025-12-17 19:00:35 -0300

    Optimizacion de consultas

--- SigemiApplication/src/main/java/com/sigemi/SigemiApplication/Repository/OrdenMantenimientoRepository.java
+++ SigemiApplication/src/main/java/com/sigemi/SigemiApplication/Repository/OrdenMantenimientoRepository.java
@@ -2,12 +2,17 @@
 package com.sigemi.SigemiApplication.Repository;
 
 import com.sigemi.SigemiApplication.Entidades.OrdenMantenimiento;
+import com.sigemi.SigemiApplication.Enums.EstadoOrden;
+import java.time.LocalDate;
 import java.util.List;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 
 public interface OrdenMantenimientoRepository extends JpaRepository<OrdenMantenimiento, Long> {
+    @Query("SELECT o FROM OrdenMantenimiento o WHERE o.fechaInicio >= :inicio AND o.fechaInicio <= :fin AND o.estado = :estado")
+    List<OrdenMantenimiento> buscarPorFechaYEstado(LocalDate inicio, LocalDate fin, EstadoOrden estado);
     // List<OrdenMantenimiento> findByEquipo_IdEquipo(Long idEquipo);
     
     @EntityGraph(attributePaths = {"equipo", "supervisor", "tareas", "repuestosUtilizados"})
