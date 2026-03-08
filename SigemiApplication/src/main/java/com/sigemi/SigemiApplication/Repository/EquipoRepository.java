47c8c0e242d2f7d6e9b5045e2c8d22513d9adef6
francoycc <francoycc@gmail.com>
2026-03-08 20:15:02 -0300

    Correcion N+1 de consultas findAll en EquipoRepository

--- SigemiApplication/src/main/java/com/sigemi/SigemiApplication/Repository/EquipoRepository.java
+++ SigemiApplication/src/main/java/com/sigemi/SigemiApplication/Repository/EquipoRepository.java
@@ -1,10 +1,16 @@
 package com.sigemi.SigemiApplication.Repository;
 
 import com.sigemi.SigemiApplication.Entidades.Equipo;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 
 public interface EquipoRepository extends JpaRepository<Equipo, Long> {
     boolean existsByCodigoEquipo(String codigoEquipo);
     boolean existsByNumeroSerie(String numeroSerie);
+    
+    @EntityGraph(attributePaths = {"ubicacionTecnica"})
+    Page<Equipo> findAll(Pageable pageable);
 }
