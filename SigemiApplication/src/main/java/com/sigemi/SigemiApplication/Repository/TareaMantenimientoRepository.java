b8c6dc1587d5ebdd37614e337ad60a5b3a5e4ecf
francoycc <francoycc@gmail.com>
2026-03-21 18:12:05 -0300

    ABM tareas construccion1

--- SigemiApplication/src/main/java/com/sigemi/SigemiApplication/Repository/TareaMantenimientoRepository.java
+++ SigemiApplication/src/main/java/com/sigemi/SigemiApplication/Repository/TareaMantenimientoRepository.java
@@ -8,13 +8,15 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 
 public interface TareaMantenimientoRepository extends JpaRepository<TareaMantenimiento, Long> {
-    @EntityGraph(attributePaths = {"equipo"})
+    @EntityGraph(attributePaths = {"orden", "tecnico"})
     List<TareaMantenimiento> findAll();
     
 //    Para cuando necesites ver el historial de un equipo específico
 //    List<TareaMantenimiento> findByEquipo_IdEquipo(Long idEquipo);
+    @EntityGraph(attributePaths = {"orden", "tecnico"})
     List<TareaMantenimiento> findByOrden_IdOrden(Long idOrden);
     
     //Busca todas las tareas asignadas a un tecnico especifico.
+    @EntityGraph(attributePaths = {"orden", "tecnico"})
     List<TareaMantenimiento> findByTecnico_IdUsuario(Long idTecnico);
 }
